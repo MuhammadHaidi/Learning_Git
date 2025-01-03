5f70da9572aefb60673b2283d8d8081555cb3718
MuhammadHaidi <mhaidi595@gmail.com>
2025-01-03 21:37:13 +0800

    First commit

---
+++ Penjumlahan.py
@@ -0,0 +1,6 @@
+def penjumlahan () :
+    a=int(input("Masukkan angka pertama: "))
+    b=int(input("Masukkan angka kedua: "))
+    return a+b
+# Jika Sudah klik Ctrl + S untuk save file 
+penjumlahan()
